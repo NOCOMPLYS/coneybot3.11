a9abbcaccc78a1b5fb7abbef5fa2cb36d7d78554
Nicolas Miroshnikov <74566080+NOCOMPLYS@users.noreply.github.com>
2024-09-26 18:26:25 +0300

    Update client_handlers.py

--- handlers/client_handlers.py
+++ handlers/client_handlers.py
@@ -50,13 +50,7 @@ async def send_mentor(message: Message):
         if False:
             pass
         else:
-            current_mentor = db.get_current_mentor()
-            current_mentor_nick = current_mentor[0]
-            current_mentor_name = current_mentor[1]
-            await message.answer("current_mentor_nick: " + f"{str(current_mentor_nick)}")
-            await message.answer("current_mentor_name: " + f"{str(current_mentor_name)}")
             db.change_current_mentor()
-            #await message.answer("nick: " + f"{str(nick)}")
             await message.answer(f"Я вижу вы готовы перейти к заработку. Вашим личным менеджером будет <a href='https://t.me/{current_mentor_nick}'><i><b>{current_mentor_name}</b></i></a>", parse_mode='html')
     else:
         await message.answer('Уже скоро. Сперва скажите как вас зовут')
