0a42bad8bef1c86836f4b9a6870dc835ea2d9c0e
Nicolas Miroshnikov <74566080+NOCOMPLYS@users.noreply.github.com>
2024-09-26 12:37:53 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from handlers.client_handlers import BOT_TOKEN
 
 # инициализируем бота и диспетчера для работы с ним
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token='7554613342:AAHq7G6wfPpUpNc8TsHCdrffzu4lJ-sJrjo', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
